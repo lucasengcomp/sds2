f30e4fbbade697043acaefc63c8d46b8cff0ac47
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2021-01-10 08:48:39 -0300

    Front-end adicionado

---
+++ front-web/src/App.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import './App.css';
+import './Hello';
+
+function App() {
+
+  useEffect(() => {
+    
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Hello :)</h1>
+    </div>
+  );
+}
+
+export default App;
